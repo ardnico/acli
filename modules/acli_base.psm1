fd72f40778c4b3e2ec6f972e9818a0f50fb30b27
ardnico <leaf.sun2@gmail.com>
2021-06-28 20:11:42 +0900

    commit2021062801

--- modules/acli_base.psm1
+++ modules/acli_base.psm1
@@ -21,12 +21,40 @@ class acli_base{
     set_param(
         [String]$resourcegroupname,
         [String]$resources,
-        [String]$type
+        [String]$keyname
+        [String]$type,
+        [int]$flag # 1: Multiple resources 2: single resource
     ){
         # Check the parameter
-        [Array]$resource_type_list = (az resource list).type
+        [Array]$resource_type_list = (az resource list -g $resourcegroupname | ConvertFrom-Json).type
+        if($? -eq $False){
+            Write_oh("The resourcegroup name likes mistaken : $resourcegroupname")
+            common_exit 1
+        }
         if($resource_type_list.IndexOf($type) -eq -1){
-            Write_oh("")
+            Write_oh("There is no such type resource $type")
+            common_exit 1
+        }
+        if($flag -eq 1){
+            $reference_test = az resource show -g $resourcegroupname -n $resources
+            if($? -eq $False){
+                Write_oh("please review the parameter")
+                common_exit 1
+            }else{
+                input_data.Add($keyname,$resources)
+            }
+        }elseif($flag -eq 2){
+            foreach($name in $resources.split(",")){
+                $reference_test = az resource show -g $resourcegroupname -n $name
+                if($? -eq $False){
+                    Write_oh("please review the parameter")
+                    common_exit 1
+                }
+            }
+            input_data.Add($keyname,$resources.split(","))
+        }else{
+            Write_oh("Sorry, please select the number '1' or '2' at the 5th potision.")
+            common_exit 1
         }
     }
 }
