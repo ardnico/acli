eb602d6fea39df0cd3361e726e34ee85dab2bc1c
ardnico <leaf.sun2@gmail.com>
2021-06-29 21:17:25 +0900

    commit 2021062901

--- modules/acli_base.psm1
+++ modules/acli_base.psm1
@@ -18,12 +18,12 @@ function common_exit($code){
 class acli_base{
     [System.Object]$global:input_data = @{}
 
-    set_param(
-        [String]$resourcegroupname,
+    # set varaiables
+    set_resources(
         [String]$resources,
+        [String]$resourcegroupname,
         [String]$keyname
-        [String]$type,
-        [int]$flag # 1: Multiple resources 2: single resource
+        [String]$type
     ){
         # Check the parameter
         [Array]$resource_type_list = (az resource list -g $resourcegroupname | ConvertFrom-Json).type
@@ -35,26 +35,83 @@ class acli_base{
             Write_oh("There is no such type resource $type")
             common_exit 1
         }
-        if($flag -eq 1){
-            $reference_test = az resource show -g $resourcegroupname -n $resources
+        foreach($name in $resources.split(",")){
+            $reference_test = az resource show -g $resourcegroupname -n $name
             if($? -eq $False){
                 Write_oh("please review the parameter")
                 common_exit 1
-            }else{
-                input_data.Add($keyname,$resources)
             }
-        }elseif($flag -eq 2){
-            foreach($name in $resources.split(",")){
-                $reference_test = az resource show -g $resourcegroupname -n $name
-                if($? -eq $False){
-                    Write_oh("please review the parameter")
-                    common_exit 1
-                }
-            }
-            input_data.Add($keyname,$resources.split(","))
-        }else{
-            Write_oh("Sorry, please select the number '1' or '2' at the 5th potision.")
+        }
+        # set parameter
+        $this.input_data.Add($keyname,$resources.split(","))
+    }
+
+    set_resource(
+        [String]$resource,
+        [String]$resourcegroupname,
+        [String]$keyname
+        [String]$type
+    ){
+        # Check the parameter
+        [Array]$resource_type_list = (az resource list -g $resourcegroupname | ConvertFrom-Json).type
+        if($? -eq $False){
+            Write_oh("The resourcegroup name likes mistaken : $resourcegroupname")
             common_exit 1
         }
+        if($resource_type_list.IndexOf($type) -eq -1){
+            Write_oh("There is no such type resource $type")
+            common_exit 1
+        }
+        $reference_test = az resource show -g $resourcegroupname -n $resource
+        if($? -eq $False){
+            Write_oh("please review the parameter")
+            common_exit 1
+        }else{
+            # set parameter
+            $this.input_data.Add($keyname,$resource)
+        }
+    }
+
+    set_vms([String]$resources, [String]$resourcegroupname){
+        $keyname = "vms"
+        $type = "Microsoft.Compute/virtualMachines"
+        $this.set_resources(
+            [String]$resources,
+            [String]$resourcegroupname,
+            [String]$keyname
+            [String]$type
+        )
+    }
+
+    set_appgw([String]$resource, [String]$resourcegroupname){
+        $keyname = "appgw"
+        $type = "Microsoft.Network/applicationGateways"
+        $this.set_resource(
+            [String]$resource,
+            [String]$resourcegroupname,
+            [String]$keyname
+            [String]$type
+        )
+    }
+
+    azlogin($env){
+        $account = az account show | ConvertFrom-Json
+        if($? -eq $False){
+            # az login method
+            az login
+        }
+        $this.input_data.Add("env",$env)
+    }
+
+    azlogin($env){
+        $account = az account show | ConvertFrom-Json
+        if($? -eq $False){
+            New-Item -ItemType Directory -Force "credential"
+            $pass_file = ".\"
+            # login method
+            if(Test-Path )
+            $login_files = 
+        }
+        $this.input_data.Add("env",$env)
     }
 }
