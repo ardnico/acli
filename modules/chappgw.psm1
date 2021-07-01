af54bfc085181b43ced8b50f66d687754fa6d7c1
ardnico <leaf.sun2@gmail.com>
2021-07-02 01:01:12 +0900

    commit2021070201

--- modules/chappgw.psm1
+++ modules/chappgw.psm1
@@ -1,12 +1,13 @@
 using Module ./acli_base.psm1
 
 class azvm : acli_base{
-    chappgwsize($){
+    chappgwsize($size,$instance){
         if(($this.input_data.rg.Length -eq 0) -and ($this.input_data.appgw.Length -eq 0)){
             Write_oh("Parameter isn't still set")
             common_exit 1
         }
         $appgw = az network application-gateway show -g $this.input_data.rg -n $this.input_data.appgw | ConvertFrom-Json
     }
+    
 
 }
